90375a677b542f37f4f9d04d55736a056f15b777
Stefano Carmignola <stefano.carmignola@tot.money>
2022-11-07 17:09:52 +0100

    move DOM change to wrapper atom

--- pages/components/themeAtom.tsx
+++ pages/components/themeAtom.tsx
@@ -1,25 +1,47 @@
-import { useAtom } from 'jotai'
+import { atom, useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
-import { useEffect } from 'react'
 import * as IO from '@fp/IO'
+import * as O from 'fp-ts/Option'
+import * as IOO from 'fp-ts/IOOption'
+import * as DOM from 'fp-ts-std/DOM'
+import { pipe } from 'fp-ts/function'
 
 type SupportedTheme = 'dark' | 'light'
 
-const updateDataTheme =
-  (theme: SupportedTheme): IO.IO<void> =>
-  () =>
-    (document.body.dataset.theme = theme)
+const setDataset =
+  <A extends string>(name: string, value: A) =>
+  (htlmElement: HTMLElement): IO.IO<A> =>
+  () => {
+    htlmElement.dataset[name] = value
+    return value
+  }
 
-export const themeAtom = atomWithStorage<SupportedTheme>(
+const updateDOMDataTheme = (theme: SupportedTheme): IO.IO<SupportedTheme> =>
+  pipe(
+    document,
+    DOM.querySelector('body'),
+    IOO.chainOptionK((e) => (e instanceof HTMLElement ? O.some(e) : O.none)),
+    IOO.chainIOK(setDataset('theme', theme)),
+    IO.map(O.getOrElse(() => theme)),
+  )
+
+export const baseThemeAtom = atomWithStorage<SupportedTheme>(
   'themePreference',
   'light',
 )
 
+const themeAtom = atom(
+  (get) => get(baseThemeAtom),
+  (_get, set, newValue: SupportedTheme) =>
+    pipe(
+      updateDOMDataTheme(newValue),
+      IO.chain((theme) => () => set(baseThemeAtom, theme)),
+      IO.execute,
+    ),
+)
+
 export const ThemeToggle: React.FC = () => {
   const [theme, setTheme] = useAtom(themeAtom)
-  useEffect(() => {
-    updateDataTheme(theme)()
-  }, [theme])
 
   return (
     <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
